3cc3404f1c88d54d0a16d783efdf7d92ed0f7c8f
Sanchay1054 <sanchaym.22cse@kongu.edu>
2024-12-07 23:57:16 +0530

    netlify files

--- functions/register.js
+++ functions/register.js
@@ -1,4 +1,4 @@
 const serverless = require('serverless-http');
-const app = require('../api/app'); // Make sure the path is correct
+const app = require('../app'); // Make sure the path is correct
 
 module.exports.handler = serverless(app);
