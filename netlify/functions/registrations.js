6769e552426849bc7fbdf0bce82b06e8a4861217
Sanchay1054 <sanchaym.22cse@kongu.edu>
2024-12-08 00:22:08 +0530

    final netlofy new

---
+++ netlify/functions/registrations.js
@@ -0,0 +1,22 @@
+const { Handler } = require('@netlify/functions');
+const mongoose = require('mongoose');
+const Registration = require('../register');
+const connectDB = require('../db');
+
+const handler = async (event, context) => {
+  try {
+    await connectDB();
+    const registrations = await Registration.find();
+    return {
+      statusCode: 200,
+      body: JSON.stringify(registrations),
+    };
+  } catch (err) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ error: 'Error fetching registrations', details: err.message }),
+    };
+  }
+};
+
+module.exports.handler = handler;
