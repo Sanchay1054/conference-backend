0a559513b8bf081e29eacc15abc4b0116294496b
Sanchay1054 <sanchaym.22cse@kongu.edu>
2024-12-08 00:52:09 +0530

    test

--- db.js
+++ db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
     try {
-        await mongoose.connect("mongodb+srv://sanchay:13-Mar-05@cluster0.jfxekhs.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0");
+        await mongoose.connect("mongodb+srv://sanchay:13-Mar-05@cluster0.jfxekhs.mongodb.net/test?retryWrites=true&w=majority&appName=Cluster0");
         console.log('MongoDB Connected');
     } catch (err) {
         console.error(err.message);
